aac84598166aa84a5adde89e70026f7a9c92e5e1
Theo <ekunnusitheophilus@gmail.com>
2023-08-06 22:06:05 +0100

    arch studio design systems

---
+++ src/shared/typography/index.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const HeadingXL = styled.h1`
+  font-family: var(--font-league-spartans);
+  font-size: 250px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 200px;
+  letter-spacing: -5px;
+`;
+
+export const HeadingL = styled.h2`
+  font-family: var(--font-league-spartans);
+  font-size: 96px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 80px;
+  letter-spacing: -2px;
+`;
+
+export const HeadingM = styled.h3`
+  font-family: var(--font-league-spartans);
+  font-size: 72px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 64px;
+  letter-spacing: -2px;
+`;
+
+export const HeadingS = styled.h4`
+  font-family: var(--font-league-spartans);
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 40px;
+`;
+
+export const BodyText = styled.p`
+  font-family: var(--font-league-spartans);
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+`;
---
+++ src/utils/devices.ts
@@ -0,0 +1,30 @@
+export const size = {
+  xs: '320px',
+  sm: '375px',
+  base: '425px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1440px',
+  xxl: '2560px',
+};
+
+export const device = {
+  xs: `(min-width: ${size.xs})`,
+  sm: `(min-width: ${size.sm})`,
+  base: `(min-width: ${size.base})`,
+  md: `(min-width: ${size.md})`,
+  lg: `(min-width: ${size.lg})`,
+  xl: `(min-width: ${size.xl})`,
+  xxl: `(min-width: ${size.xxl})`,
+};
+
+export const radius = {
+  sm: `0.1875rem`,
+  md: `0.375rem`,
+};
+
+export const transition = `transition-property: all;
+      transition-duration: 300ms;
+      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);`;
+
+export const shadow = ` box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1);`;
